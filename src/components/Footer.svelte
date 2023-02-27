d18654586f0f3d7d38260544df3c43b34c52d2cf
aaronkai <aaron.kai.hubbard@gmail.com>
2023-02-27 15:49:11 -0500

    6 files modified, 1 added, 3 deleted

--- src/components/Footer.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-	import SocialMedia from './SocialMedia.svelte';
-</script>
-
-<footer class="flex justify-between px-4 py-2 border-t-4 shadow border-zinc-900">
-	<p>2023</p>
-	<SocialMedia size={2} />
-	<a href="https://aaronhubbard.dev">akh dev</a>
-</footer>
