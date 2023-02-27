5cec704376e70d941ed9c7be67bb68878b79231e
aaronkai <aaron.kai.hubbard@gmail.com>
2023-02-27 15:04:37 -0500

    1 binaries uploaded

---
+++ src/components/DayPicker.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+	export let uniqueDays: string[];
+	import { userPickedDay } from '$lib/stores.js';
+
+	let pickedDay: string;
+
+	userPickedDay.subscribe((value) => {
+		pickedDay = value;
+	});
+</script>
+
+<div>
+	<p class="font-stencil text-2xl">Filter by Day</p>
+
+	<div class="flex flex-wrap gap-1">
+		<button
+			class={`${
+				'All' == pickedDay ? 'bg-red-600' : 'bg-zinc-700'
+			} px-4 py-1 text-lg rounded  text-zinc-100 font-stencil shadow`}
+			on:click={() => {
+				userPickedDay.set('All');
+			}}>All</button
+		>
+		{#each uniqueDays as day}
+			<button
+				on:click={() => {
+					userPickedDay.set(day);
+				}}
+				class={`${
+					day == pickedDay ? 'bg-red-600' : 'bg-zinc-700'
+				} px-4 py-1 text-lg rounded  text-zinc-100 font-stencil`}>{day}</button
+			>
+		{/each}
+	</div>
+</div>
---
+++ src/components/LocationPicker.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+	import { userPickedLocation } from '$lib/stores.js';
+	import type { iSchedules } from '$lib/types';
+
+	export let schedules: iSchedules;
+	const allLocations = schedules.map((schedule) => schedule.AO.neighborhood);
+	const uniqueLocations = Array.from(new Set(allLocations));
+
+	let pickedLocation: string;
+
+	userPickedLocation.subscribe((value) => {
+		pickedLocation = value;
+	});
+</script>
+
+<div>
+	<p class="font-stencil text-2xl">Filter by Location</p>
+	<div class="flex flex-wrap gap-1">
+		<button
+			class={`${
+				'All' == pickedLocation ? 'bg-red-600' : 'bg-zinc-700'
+			} px-4 py-1 text-lg rounded  text-zinc-100 font-stencil shadow`}
+			on:click={() => {
+				userPickedLocation.set('All');
+			}}>All</button
+		>
+		{#each uniqueLocations as day}
+			<button
+				on:click={() => {
+					userPickedLocation.set(day);
+				}}
+				class={`${
+					day == pickedLocation ? 'bg-red-600' : 'bg-zinc-700'
+				} px-4 py-1 text-lg rounded  text-zinc-100 font-stencil`}>{day}</button
+			>
+		{/each}
+	</div>
+</div>
