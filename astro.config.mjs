395e9f34cf3d9e323247d9f7d6aa3597cb2eb101
aaron <aaron.kai.hubbard@gmail.com>
2023-03-02 16:05:36 -0500

    working on responsive images

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,26 @@
 // https://astro.build/config
 
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-import sanity from 'astro-sanity';
-import svelte from '@astrojs/svelte';
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import sanity from "astro-sanity";
+import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), sanity({
-    projectId: '22i3wv6j',
-    dataset: 'production',
-    apiVersion: '2023-02-03',
-    useCdn: true
-  }), svelte(), image()]
+  integrations: [
+    tailwind(),
+    sanity({
+      projectId: "22i3wv6j",
+      dataset: "production",
+      apiVersion: "2023-02-03",
+      useCdn: true,
+    }),
+    svelte(),
+    image({
+      serviceEntryPoint: "@astrojs/image/sharp",
+    }),
+  ],
 });
