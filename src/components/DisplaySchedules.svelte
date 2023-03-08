3884d539811a276a3d9ad84f84800a194121afdd
aaron <aaron.kai.hubbard@gmail.com>
2023-03-08 11:22:00 -0500

    reduce text size schedule header

--- src/components/DisplaySchedules.svelte
+++ src/components/DisplaySchedules.svelte
@@ -1,53 +1,59 @@
 <script lang="ts">
-	export let schedules: iSchedules;
-	import { userPickedDay, userPickedLocation } from './stores.js';
-	import type { iSchedules } from '../types';
-	let pickedDay: string;
-	let pickedLocation: string;
+  export let schedules: iSchedules;
+  import { userPickedDay, userPickedLocation } from "./stores.js";
+  import type { iSchedules } from "../types";
+  let pickedDay: string;
+  let pickedLocation: string;
 
-	userPickedDay.subscribe((value) => {
-		pickedDay = value;
-	});
+  userPickedDay.subscribe((value) => {
+    pickedDay = value;
+  });
 
-	userPickedLocation.subscribe((value) => {
-		pickedLocation = value;
-	});
+  userPickedLocation.subscribe((value) => {
+    pickedLocation = value;
+  });
 
-	let displayedSchedules = schedules;
+  let displayedSchedules = schedules;
 
-	$: if (pickedDay == 'All' && pickedLocation == 'All') {
-		displayedSchedules = schedules;
-	} else if (pickedDay != 'All' && pickedLocation != 'All') {
-		displayedSchedules = schedules
-			.filter((schedule) => schedule.day.includes(pickedDay))
-			.filter((schedule) => schedule.AO.neighborhood.includes(pickedLocation));
-	} else if (pickedDay != 'All') {
-		displayedSchedules = schedules.filter((schedule) => schedule.day.includes(pickedDay));
-	} else if (pickedLocation != 'All') {
-		displayedSchedules = schedules.filter((schedule) =>
-			schedule.AO.neighborhood.includes(pickedLocation)
-		);
-	}
+  $: if (pickedDay == "All" && pickedLocation == "All") {
+    displayedSchedules = schedules;
+  } else if (pickedDay != "All" && pickedLocation != "All") {
+    displayedSchedules = schedules
+      .filter((schedule) => schedule.day.includes(pickedDay))
+      .filter((schedule) => schedule.AO.neighborhood.includes(pickedLocation));
+  } else if (pickedDay != "All") {
+    displayedSchedules = schedules.filter((schedule) =>
+      schedule.day.includes(pickedDay)
+    );
+  } else if (pickedLocation != "All") {
+    displayedSchedules = schedules.filter((schedule) =>
+      schedule.AO.neighborhood.includes(pickedLocation)
+    );
+  }
 </script>
 
 {#if displayedSchedules && displayedSchedules.length}
-	{#each displayedSchedules as schedule}
-		<div class="grid p-6 mb-4 border-4 rounded shadow bg-zinc-300 border-zinc-900 grow ">
-			<h2 class="pb-2">{schedule.AO.name}</h2>
-			<div class="flex flex-col gap-2 ">
-				<div class="flex">
-					<p>Days:</p>
-					{#each schedule.day as day}
-						<span class="px-2 underline underline-offset-2 font-bold">{day}</span>
-					{/each}
-				</div>
-				<p>Style: {schedule.style}</p>
-				<p>Neighborhood: {schedule.AO.neighborhood}</p>
-				<p>Start Time: {schedule.start_time}</p>
-				<p>Location: {schedule.AO.location_name}</p>
-			</div>
-		</div>
-	{/each}
+  {#each displayedSchedules as schedule}
+    <div
+      class="grid p-6 mb-4 border-4 rounded shadow bg-zinc-300 border-zinc-900 grow "
+    >
+      <h2 class="pb-2 text-4xl mb-2">{schedule.AO.name}</h2>
+      <div class="flex flex-col gap-2 ">
+        <div class="flex">
+          <p>Days:</p>
+          {#each schedule.day as day}
+            <span class="px-2 underline underline-offset-2 font-bold"
+              >{day}</span
+            >
+          {/each}
+        </div>
+        <p>Style: {schedule.style}</p>
+        <p>Neighborhood: {schedule.AO.neighborhood}</p>
+        <p>Start Time: {schedule.start_time}</p>
+        <p>Location: {schedule.AO.location_name}</p>
+      </div>
+    </div>
+  {/each}
 {:else}
-	<h2 class="text-red-600">No schedules match filter.</h2>
+  <h2 class="text-red-600">No schedules match filter.</h2>
 {/if}
