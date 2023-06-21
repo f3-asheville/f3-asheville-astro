cffe9df3050d340a8b395129aa61d2d9798087c4
aaron <aaron.kai.hubbard@gmail.com>
2023-06-21 10:59:26 -0400

    moved map ownership to f3 avl account

--- src/components/LocationMap.svelte
+++ src/components/LocationMap.svelte
@@ -11,42 +11,42 @@
   {#if pickedLocation == "All"}
     <iframe
       title="all locations map"
-      src="https://www.google.com/maps/d/u/0/embed?mid=1vu_HRJFd93InZVySmqwyPe3wbZ72czA&ehbc=2E312F"
+      src="https://www.google.com/maps/d/embed?mid=1e711p6a2abr2rs0JD12bnThG6kobVB8&ehbc=2E312F"
       width="640"
       class="w-full h-full"
     />
   {:else if $userPickedLocation == "North Asheville"}
     <iframe
       title="Northbound Map"
-      src="https://www.google.com/maps/d/u/0/embed?mid=1oFXgJhnTG1X4N6YFDzocLJf7Aj_PueY&ehbc=2E312F"
+      src="https://www.google.com/maps/d/embed?mid=1_RbBX_fHWEHjGSgfcfIsowp3IrODuXo&ehbc=2E312F"
       width="640"
       class="w-full h-full"
     />
   {:else if $userPickedLocation == "Black Mountain"}
     <iframe
       title="Black Mountain"
-      src="https://www.google.com/maps/d/u/0/embed?mid=1C2j2sRGt3_c_9jVl6dTUCjavqiEBQew&ehbc=2E312F"
+      src="https://www.google.com/maps/d/embed?mid=1KVPKT4rqRnEHP32Yly4qyIlmBKTplHU&ehbc=2E312F"
       width="640"
       class="w-full h-full"
     />
   {:else if $userPickedLocation == "Downtown Asheville"}
     <iframe
       title="Downtown"
-      src="https://www.google.com/maps/d/u/0/embed?mid=1KK6_jER9onG9JgvscIgPlghYuqwS8vc&ehbc=2E312F"
+      src="https://www.google.com/maps/d/embed?mid=18W43GG3ZfZfWuAq663x028qDPWDoc8o&ehbc=2E312F"
       width="640"
       class="w-full h-full"
     />
   {:else if $userPickedLocation == "South Asheville"}
     <iframe
       title="South"
-      src="https://www.google.com/maps/d/u/0/embed?mid=1HBkrrzrfBAMtnyPiPRHclt8vNgZj4_c&ehbc=2E312F"
+      src="https://www.google.com/maps/d/embed?mid=1UB0IHc-FtjIe7MW8E92Jd6pG_0A5CsY&ehbc=2E312F"
       width="640"
       class="w-full h-full"
     />
   {:else}
     <iframe
       title="all locations map"
-      src="https://www.google.com/maps/d/u/0/embed?mid=1vu_HRJFd93InZVySmqwyPe3wbZ72czA&ehbc=2E312F"
+      src="https://www.google.com/maps/d/embed?mid=1e711p6a2abr2rs0JD12bnThG6kobVB8&ehbc=2E312F"
       width="640"
       class="w-full h-full"
     />
